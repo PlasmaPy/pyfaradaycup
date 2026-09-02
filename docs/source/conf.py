87a8e50d7188ed41aba144540356695ffd7f76d4
Nick Murphy <namurphy@cfa.harvard.edu>
2026-09-02 18:20:25 -0400

    Add commented out list of Sphinx extensions
    This comes from PlasmaPy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,35 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    # plasmapy extensions & setups
+    #"plasmapy_sphinx.theme",
+    #"plasmapy_sphinx.ext.autodoc",
+    #"plasmapy_sphinx.ext.directives",
+    # other 3rd party extensions
+    #"IPython.sphinxext.ipython_console_highlighting",
+    #"nbsphinx",
+    #"notfound.extension",
+    #"sphinx.ext.duration",
+    #"sphinx.ext.extlinks",
+    #"sphinx.ext.graphviz",
+    #"sphinx.ext.intersphinx",
+    #"sphinx.ext.mathjax",
+    #"sphinx.ext.napoleon",
+    #"sphinx.ext.todo",
+    #"sphinx.ext.viewcode",
+    #"sphinx_changelog",
+    #"sphinx_copybutton",
+    #"sphinx_gallery.load_style",
+    #"sphinx_issues",
+    #"sphinx_reredirects",
+    #"sphinx_tabs.tabs",
+    #"sphinx_toolbox.collapse",
+    #"sphinx_toolbox.rest_example",
+    #"sphinxcontrib.bibtex",
+    #"sphinxemoji.sphinxemoji",
+    #"sphinxcontrib.globalsubs",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
