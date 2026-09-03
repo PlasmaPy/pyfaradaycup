34de28db2d7f1aa2946ccb659b27299577a8c706
Nick Murphy <namurphy@cfa.harvard.edu>
2026-09-02 20:01:28 -0400

    Remove intentional error in utils.placeholder
    I put the error in to make sure that doctests were being run.

--- src/pyfaradaycup/utils/__init__.py
+++ src/pyfaradaycup/utils/__init__.py
@@ -5,13 +5,13 @@ __all__: list[str] = ["placeholder"]
 from typing import Literal
 
 
-def placeholder(x: int) -> Literal[42]:
+def placeholder() -> Literal[42]:
     """
     Run a placeholder function.
 
     Examples
     --------
-    >>> placeholder(1)  # intentional failure
-    43
+    >>> placeholder()
+    42
     """
     return 42
